a3fba0101ca50b0bfad944dbee43f37d06fc6f73
Doa Bonifacio <doa.bonifacio@alliander.com>
2024-08-28 09:47:48 +0200

    Clean intial setup

---
+++ lesmateriaal/art.ts
@@ -0,0 +1,38 @@
+let developers = [
+	{
+		id: 'AL00001',
+		voornaam: 'Bob',
+		achternaam: 'de Bouwer',
+		salaris: 5000,
+		expertise: 'FE development in TS en svelte',
+		avatar: '/man.png',
+		pluim: false
+	},
+	{
+		id: 'AL00002',
+		voornaam: 'Lisa',
+		achternaam: 'Simpson',
+		salaris: 7000,
+		expertise: 'BE development in NodeJS',
+		avatar: '/vrouw.png',
+		pluim: false
+	},
+	{
+		id: 'AL00003',
+		voornaam: 'Jane',
+		achternaam: 'Doe',
+		salaris: 8000,
+		expertise: 'Lead developer',
+		avatar: '/vrouw.png',
+		pluim: false
+	},
+	{
+		id: 'AL00004',
+		voornaam: 'Hannibal',
+		achternaam: 'Smith',
+		salaris: 9000,
+		expertise: 'Architect',
+		avatar: '/man.png',
+		pluim: false
+	},
+]
