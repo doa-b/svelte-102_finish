3662f3819f51559f79e0c05f7c9ce2dec899c8a1
Doa Bonifacio <doa.bonifacio@alliander.com>
2024-08-28 11:07:56 +0200

    Voeg een lijstje developers toe

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,46 @@
 <script lang="ts">
 
-	import Developer from "./lib/Developer.svelte";
 	import DeveloperLijst from "./lib/DeveloperLijst.svelte";
+	import type {Developer} from "./types/developer";
+
+	let developers: Array<Developer> = [
+		{
+			id: 'AL00001',
+			voornaam: 'Bob',
+			achternaam: 'de Bouwer',
+			salaris: 5000,
+			expertise: 'FE development in TS en svelte',
+			avatar: '/man.png',
+			pluim: false
+		},
+		{
+			id: 'AL00002',
+			voornaam: 'Lisa',
+			achternaam: 'Simpson',
+			salaris: 7000,
+			expertise: 'BE development in NodeJS',
+			avatar: '/vrouw.png',
+			pluim: false
+		},
+		{
+			id: 'AL00003',
+			voornaam: 'Jane',
+			achternaam: 'Doe',
+			salaris: 8000,
+			expertise: 'Lead developer',
+			avatar: '/vrouw.png',
+			pluim: false
+		},
+		{
+			id: 'AL00004',
+			voornaam: 'Hannibal',
+			achternaam: 'Smith',
+			salaris: 9000,
+			expertise: 'Architect',
+			avatar: '/man.png',
+			pluim: false
+		},
+	]
 </script>
 
 <main>
