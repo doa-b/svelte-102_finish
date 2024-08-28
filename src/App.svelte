88e8864defb5b6ed0042c21b9f0fb75f0117c30a
Doa Bonifacio <doa.bonifacio@alliander.com>
2024-08-28 11:02:03 +0200

    Voeg DeveloperLijst component toe met 4 developers

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 
 	import Developer from "./lib/Developer.svelte";
+	import DeveloperLijst from "./lib/DeveloperLijst.svelte";
 </script>
 
 <main>
     <h1> Welkom TypeScripters</h1>
-    <div>
-        <Developer voornaam="Lisa" achternaam="Simpson" expertise="Backend developer in NodeJS" avatar="/vrouw.png"/>
+      <div>
+       <DeveloperLijst/>
     </div>
 </main>
 
---
+++ src/lib/DeveloperLijst.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+import Developer from "./Developer.svelte";
+</script>
+
+<div>
+    <Developer voornaam="Lisa" achternaam="Simpson" expertise="Backend developer in NodeJS" avatar="/vrouw.png"/>
+    <Developer voornaam="Lisa" achternaam="Simpson" expertise="Backend developer in NodeJS" avatar="/vrouw.png"/>
+    <Developer voornaam="Lisa" achternaam="Simpson" expertise="Backend developer in NodeJS" avatar="/vrouw.png"/>
+    <Developer voornaam="Lisa" achternaam="Simpson" expertise="Backend developer in NodeJS" avatar="/vrouw.png"/>
+</div>
+
+<style>
+</style>
