62de947eb7c6974fef7790347d44f1d2b0f50c54
Doa Bonifacio <doa.bonifacio@alliander.com>
2024-08-28 10:31:59 +0200

    Made Bob a little smaller. Explained style incapsulation

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 
-import Developer from "./lib/Developer.svelte";
+	import Developer from "./lib/Developer.svelte";
 </script>
 
 <main>
- <h1> Welkom TypeScripters</h1>
- <Developer/>
+    <h1> Welkom TypeScripters</h1>
+    <div>
+        <Developer/>
+    </div>
 </main>
 
 <style>
+ div {
+  width: 250px;
+ }
 </style>
