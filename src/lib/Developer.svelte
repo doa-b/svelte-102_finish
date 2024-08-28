b73f970b6ed575600e12f1e15f93ed396853f765
Doa Bonifacio <doa.bonifacio@alliander.com>
2024-08-28 10:26:28 +0200

    basic bob de bouwer

--- src/lib/Developer.svelte
+++ src/lib/Developer.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-
+    let naam = 'Bob de Bouwer';
+	let expertise = 'FE development in TS en svelte';
+	let salaris = 5000;
+	let avatar = '/man.png'
 </script>
 
 <div>
-
+    <img src={avatar} alt="Avatar">
+    <h4>{naam} {salaris}€</h4>
+    <p>{expertise}</p>
 </div>
 
 <style>
